364969e6b420a4775b9a492fa3f39673ab66e73e
Saadelquaul <Saadelquaul@gmail.com>
2025-11-24 15:06:44 +0100

    initial commit

---
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/MicroTechSmartShopApplication.java
@@ -0,0 +1,13 @@
+package com.SmartShop.MicroTech_SmartShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTechSmartShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTechSmartShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartShop/MicroTech_SmartShop/MicroTechSmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartShop.MicroTech_SmartShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTechSmartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
