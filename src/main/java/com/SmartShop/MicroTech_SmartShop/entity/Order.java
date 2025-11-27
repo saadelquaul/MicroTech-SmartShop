a5f21705fcd74d4c3799fe63ec17a0d1a4ba575a
Saadelquaul <Saadelquaul@gmail.com>
2025-11-27 09:37:52 +0100

    add List import to Order entity for improved functionality

--- src/main/java/com/SmartShop/MicroTech_SmartShop/entity/Order.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/entity/Order.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
