de9f736d9bd028915df146891f97440ed5b038d7
Saadelquaul <Saadelquaul@gmail.com>
2025-12-02 14:10:15 +0100

    add BusinessException for handling business logic errors

---
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.SmartShop.MicroTech_SmartShop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+public class BusinessException extends RuntimeException {
+}
