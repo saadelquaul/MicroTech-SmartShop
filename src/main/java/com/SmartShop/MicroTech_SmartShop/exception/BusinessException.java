939f110d1fe15a9954fc0df76f2a3decaba8326b
Saadelquaul <Saadelquaul@gmail.com>
2025-12-02 14:27:55 +0100

    add constructor to BusinessException for custom error messages

--- src/main/java/com/SmartShop/MicroTech_SmartShop/exception/BusinessException.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/exception/BusinessException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
 public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
 }
