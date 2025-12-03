cc17a08ff649f4764a2692ab3493dbaa3c1c6bcc
Saadelquaul <Saadelquaul@gmail.com>
2025-12-03 12:08:34 +0100

    add UserMapper to handle user entity and DTO conversions

---
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.SmartShop.MicroTech_SmartShop.mapper;
+
+
+import com.SmartShop.MicroTech_SmartShop.dto.request.ClientRequestDto;
+import com.SmartShop.MicroTech_SmartShop.dto.request.LoginRequest;
+import com.SmartShop.MicroTech_SmartShop.dto.response.AuthResponse;
+import com.SmartShop.MicroTech_SmartShop.entity.User;
+import com.SmartShop.MicroTech_SmartShop.enums.UserRole;
+import com.SmartShop.MicroTech_SmartShop.utils.PasswordUtil;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public AuthResponse toDTO (User user) {
+        if (user == null) return null;
+
+        return AuthResponse.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+    }
+
+    public User toEntity(ClientRequestDto clientRequestDto) {
+        if (clientRequestDto == null) return null ;
+
+        return User.builder()
+                .username(clientRequestDto.getUsername())
+                .password(PasswordUtil.hash(clientRequestDto.getPassword()))
+                .role(UserRole.CLIENT)
+                .build();
+    }
+}
