76505652ce3f1e1deffc780ec7139f2cb1ad4354
Saadelquaul <Saadelquaul@gmail.com>
2025-12-03 12:07:52 +0100

    update BusinessException response status to UNPROCESSABLE_ENTITY

--- src/main/java/com/SmartShop/MicroTech_SmartShop/exception/BusinessException.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/exception/BusinessException.java
@@ -3,7 +3,7 @@ package com.SmartShop.MicroTech_SmartShop.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
