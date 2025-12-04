2b22924b2d55e745f5247b82da7df150171a02d6
Saadelquaul <Saadelquaul@gmail.com>
2025-12-04 11:47:46 +0100

    add updateClientTier method to LoyaltyService for adjusting client tiers based on orders and spending

--- src/main/java/com/SmartShop/MicroTech_SmartShop/service/LoyaltyService.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/service/LoyaltyService.java
@@ -1,6 +1,7 @@
 package com.SmartShop.MicroTech_SmartShop.service;
 
 
+import com.SmartShop.MicroTech_SmartShop.entity.Client;
 import com.SmartShop.MicroTech_SmartShop.enums.CustomerTier;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +28,20 @@ public class LoyaltyService {
                 return BigDecimal.ZERO;
         }
         return BigDecimal.ZERO;
+    }
+
+    public void updateClientTier(Client client) {
+        int orders = client.getTotalOrders();
+        BigDecimal spent = client.getTotalSpent();
+
+        if (orders > 20 || spent.compareTo(new BigDecimal("15000")) > 0) {
+            client.setTier(CustomerTier.PLATINUM);
+        } else if (orders > 10 || spent.compareTo(new BigDecimal("5000")) > 0) {
+            client.setTier(CustomerTier.GOLD);
+        } else if (orders > 3 || spent.compareTo(new BigDecimal("1000")) > 0) {
+            client.setTier(CustomerTier.SILVER);
+        } else {
+            client.setTier(CustomerTier.BASIC);
+        }
     }
 }
