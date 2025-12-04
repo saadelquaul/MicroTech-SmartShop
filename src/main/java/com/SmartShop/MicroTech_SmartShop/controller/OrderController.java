48d4f84cd0089029273d2b3468bd9f2614fc0d46
Saadelquaul <Saadelquaul@gmail.com>
2025-12-04 12:07:02 +0100

    add createOrder method to OrderController for handling order creation requests

--- src/main/java/com/SmartShop/MicroTech_SmartShop/controller/OrderController.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/controller/OrderController.java
@@ -1,10 +1,17 @@
 package com.SmartShop.MicroTech_SmartShop.controller;
 
+import com.SmartShop.MicroTech_SmartShop.dto.request.OrderRequestDto;
+import com.SmartShop.MicroTech_SmartShop.dto.response.OrderResponseDto;
 import com.SmartShop.MicroTech_SmartShop.enums.UserRole;
 import com.SmartShop.MicroTech_SmartShop.exception.BusinessException;
 import com.SmartShop.MicroTech_SmartShop.service.OrderService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +24,11 @@ public class OrderController {
     private final OrderService orderService;
 
 
-
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody OrderRequestDto dto, HttpSession session) {
+        checkAdmin(session);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(dto));
+    }
 
     private void checkLogin(HttpSession session) {
         if (session.getAttribute("USER_ID") == null) throw new BusinessException("Unauthorized");
