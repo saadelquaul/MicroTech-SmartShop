a77c3563534165e97b5e43d311ebc16dc3b09f5d
Saadelquaul <Saadelquaul@gmail.com>
2025-12-08 11:39:31 +0100

    add findAllIncludingDeleted method to ProductRepository for retrieving all products, including deleted ones

--- src/main/java/com/SmartShop/MicroTech_SmartShop/repository/ProductRepository.java
+++ src/main/java/com/SmartShop/MicroTech_SmartShop/repository/ProductRepository.java
@@ -4,11 +4,16 @@ import com.SmartShop.MicroTech_SmartShop.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
+    @Query(value = "SELECT * FROM PRODUCT",
+            countQuery = "SELECT count(*) FROM product",
+            nativeQuery = true)
+    Page<Product> findAllIncludingDeleted(Pageable pageable);
     Page<Product> findByIsDeletedFalse(Pageable pageable);
 }
